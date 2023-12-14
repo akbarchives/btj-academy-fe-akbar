8087610c148f321701d8a2bb7fb3c043b05e7eae
akbarchives <rizqiakbar2412@gmail.com>
2023-12-14 19:10:43 +0700

    add task day 5

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-# Cara run di komputer agar berjalan dengan baik 
-- clone repository ini 
+# Cara run di komputer agar berjalan dengan baik
+
+- clone repository ini
 - `git clone https://github.com/akbarchives/btj-academy-fe-akbar.git`
 - buka project dengan vscode
 - gunakan ekstensi live server untuk menjalankan
 
 # Task Day 1
-Create About Page with Your Profile 
+
+Create About Page with Your Profile
 
 Use as many HTML tag as you can and add your profile about :
+
 - Image Profile
 - Name
 - One paragraph about yourself
@@ -15,31 +18,36 @@ Use as many HTML tag as you can and add your profile about :
 - Your Motto in Life
 
 # Task Day 2
+
 Task : "use as many CSS property as you can"
 
-## index.html 
+## index.html
+
 terdiri dari :
+
 - header
 - nav
 - section
-    - form
-    - Image
-    - article
+  - form
+  - Image
+  - article
 - footer
 
 ## about.html
+
 terdiri dari :
+
 - header
 - nav
 - section
-    - article
-        - table
-            - Image
-            - nama
-            - "about myself"
-            - Education
-            - Work
-            - Motto
+  - article
+    - table
+      - Image
+      - nama
+      - "about myself"
+      - Education
+      - Work
+      - Motto
 - footer
 
 ## Task 3
@@ -48,14 +56,26 @@ terdiri dari :
 2. Hasil Day 3 di push ke main, update readme nya juga
 3. CSS yang digunakan External CSS
 4. Ketentuan about page:
-warna color palette senada dengan home atau login page
-layout sama dengan home (background page, header, footer)
+   warna color palette senada dengan home atau login page
+   layout sama dengan home (background page, header, footer)
 
 # Task Day 4
+
 add form validation
+
 - animation on logo 360 degree
 - show password checkbox
 - field tidak boleh kosong
 - passowrd minimal 1 huruf besar, i huruf kecil, 1 number, 1 symbol
 - Capslock warning
 - redirect to about page if valid
+
+# Task Day 5
+
+- Update script JS yang dibuat sebelumnya menggunakan jQuery
+- Tambahkan penggunaan Gunakan Fake API saat submit login page
+  https://dummyjson.com/docs/users
+  https://dummyjson.com/docs/auth
+  Ketika response 200 dari API maka redirect ke about page.
+- Update the About Page from previous meeting.
+- Modify script js using jQuery, add some jQuery effect, be creative!
--- src/main.js
+++ src/main.js
@@ -1,94 +1,116 @@
 // Rotate Image
-let degree = 0;
-function rotateImage() {
-  let img = document.getElementById('brandImage');
-
-  degree += 360;
-  img.style.transform = `rotate(${degree}deg)`;
-}
+$(document).ready(() => {
+  let degree = 0;
+  $('#brandImage').click(function () {
+    degree += 360;
+    $(this).css('transform', `rotate(${degree}deg)`);
+  });
+});
 
 // show/hide password
-const passwordInput = document.getElementById('password');
-const showPasswordCheckbox = document.getElementById('showPassword');
-
-showPasswordCheckbox.addEventListener('change', function () {
-  if (this.checked) {
-    passwordInput.type = 'text';
-  } else {
-    passwordInput.type = 'password';
-  }
+$('#showPassword').on('change', function () {
+  $('#password').attr('type', $(this).prop('checked') ? 'text' : 'password');
 });
 
 // show Capslock status
-const capslockStatus = document.getElementById('capsLockWarning');
-
-passwordInput.addEventListener('keyup', function (e) {
-  if (e.getModifierState('CapsLock')) {
-    capslockStatus.style.visibility = 'visible';
+$('#password').on('keyup', function (e) {
+  if (e.originalEvent.getModifierState('CapsLock')) {
+    $('#capsLockWarning').css('visibility', 'visible');
   } else {
-    capslockStatus.style.visibility = 'hidden';
+    $('#capsLockWarning').css('visibility', 'hidden');
   }
 });
 
 // username validation
 function usernameValidation() {
-  const usernameInput = document.getElementById('username');
-  // field username
-  if (usernameInput.value == '') {
-    document.getElementById('usernameError').style.visibility = 'visible';
+  if ($('#username').val() == '') {
+    $('#usernameError').css('visibility', 'visible');
   } else {
-    document.getElementById('usernameError').style.visibility = 'hidden';
+    $('#usernameError').css('visibility', 'hidden');
   }
 }
+
 // password validation
 function passwordValidation() {
-  const passwordError = document.getElementById('passwordError');
-
-  var symbolRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+  // disable symbol
+  // var symbolRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
   var numberRegex = /[0-9]/;
   var lowercaseRegex = /[a-z]/;
   var uppercaseRegex = /[A-Z]/;
 
-  passwordInput.addEventListener('keyup', function () {
+  $('#password').on('keyup', function () {
     arrErrors = [];
-    if (!symbolRegex.test(passwordInput.value)) {
-      arrErrors.push('1 Symbol');
-    }
-    if (!numberRegex.test(passwordInput.value)) {
+    if (!numberRegex.test($(this).val())) {
       arrErrors.push('1 Number');
     }
-    if (!uppercaseRegex.test(passwordInput.value)) {
-      arrErrors.push('1 Uppercase');
+    if (!lowercaseRegex.test($(this).val())) {
+      arrErrors.push('1 uppercase');
     }
-    if (!lowercaseRegex.test(passwordInput.value)) {
-      arrErrors.push('1 Lowercase');
+    if (!uppercaseRegex.test($(this).val())) {
+      arrErrors.push('1 lowercase');
     }
 
     totalError = arrErrors.join(', ');
+
     if (arrErrors.length != 0) {
-      passwordError.style.visibility = 'visible';
-      passwordError.textContent = `Password must contain ${totalError}`;
+      $('#passwordError').css('visibility', 'visible');
+      $('#passwordError').text(`Password must contain ${totalError}`);
     } else if (arrErrors.length == 0) {
-      passwordError.style.visibility = 'hidden';
+      $('#passwordError').css('visibility', 'hidden');
     }
 
-    if (passwordInput.value == '') {
-      passwordError.style.visibility = 'visible';
-      passwordError.textContent = 'please fill password';
+    if ($('#password').value == '') {
+      $('#passwordError').css('visibility', 'visible');
+      $('#passwordError').text('please fill password');
     }
   });
 }
 
+const arrr = [30, 21];
+
 // form validation
-function validateForm(event) {
-  event.preventDefault();
+function validateForm(e) {
+  e.preventDefault();
   usernameValidation();
   passwordValidation();
 
-  var usernameError = document.getElementById('usernameError').style.visibility;
-  var passwordError = document.getElementById('passwordError').style.visibility;
+  if (
+    $('#usernameError').css('visibility') == 'hidden' &&
+    $('#passwordError').css('visibility') == 'hidden'
+  ) {
+    const username = $('#username').val();
+    const password = $('#password').val();
 
-  if (usernameError == 'hidden' && passwordError == 'hidden') {
-    window.location.href = 'about.html';
+    fetch('https://dummyjson.com/users')
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data.users[1]);
+        const users = data.users;
+        console.log(users);
+
+        // Simulating a login process (replace this with your actual login process)
+        function loginUser(username, password) {
+          const foundUser = users.find(
+            user => user.username === username && user.password === password
+          );
+
+          if (foundUser) {
+            console.log('Login successful!');
+            window.location.href = 'about.html';
+
+            // Perform actions for successful login
+          } else {
+            console.log('Invalid username or password');
+            // Handle unsuccessful login
+          }
+        }
+
+        // Example login
+        loginUser(username, password);
+      });
   }
 }
+
+fetch('https://dummyjson.com/users')
+  .then(res => res.json())
+  .then(console.log);
